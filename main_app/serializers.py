2c7fdb4edc118e9d6dd27371a41a5a6650656e8c
MohamedAmineLadjimi <ladjimi.mohamedamine@esprit.tn>
2022-04-25 23:21:38 +0100

    API exposed and working
    disease prediction API exposed using Django REST Framework.

---
+++ main_app/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import diseaseinfo
+
+
+class diseaseinfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = diseaseinfo
+        fields = ["diseasename", "confidence"]
